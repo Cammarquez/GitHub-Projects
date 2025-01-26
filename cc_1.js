7368cb489f2460143abf52bb028b6bbdbae6ad83
Cammarquez <cameronmarquez@usf.edu>
2025-01-26 12:57:19 -0500

    Task 4 - Customer Messaging

--- cc_1.js
+++ cc_1.js
@@ -24,11 +24,3 @@ console.log("New Balance:", accountBalance)
 // Task 4
 let customerName = "John Pork";
 console.log("Welcome", customerName, "to our website!");
-
-// Task 5
-let isLoggedIn = false;
-if (isLoggedIn === true) {
-    console.log("Logged in Succesful!");
-} else if (isLoggedIn === false) {
-    console.log("Please login to continue.");
-}
