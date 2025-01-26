ccad8517468137804137c89b5e126e285027a163
Cammarquez <cameronmarquez@usf.edu>
2025-01-26 13:34:36 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -4,3 +4,14 @@ console.log("Products:", products);
 products.unshift("Hat");
 products.pop();
 console.log("New Products:", products);
+
+// Task 2
+let scores = [10, 20, 30, 40, 50];
+console.log("Initial Scores:", scores);
+scores[1] = 25;
+console.log("Updated Scores:", scores);
+let total = 0;
+for (let i = 0; i < scores.length; i++) {
+    total += scores[i];}
+let average = total / scores.length;
+console.log("Average Score:", average);
