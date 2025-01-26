8f884ca81600f7cdfe967653ec8f355a2306e6f2
Cammarquez <cameronmarquez@usf.edu>
2025-01-26 12:37:09 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,11 @@ console.log("Employee Name:", employeeName, "| Type:", typeof employeeName);
 console.log("Employee ID:", employeeID, "| Type:", typeof employeeID);
 console.log("Is Active:", isActive, "| Type:", typeof isActive);
 
+// Task 2
+let productName = "Box";
+const productPrice = 10;
+var isAvailable = true;
+
+console.log("Product Name:", productName, "| Type:", typeof productName);
+console.log("Product Price:", productPrice, "| Type:", typeof productPrice);
+console.log("Is Available:", isAvailable, "| Type:", typeof isAvailable);
