48ce2e747329a21afae1b45a22518464738d5d8d
Cammarquez <cameronmarquez@usf.edu>
2025-02-01 10:03:56 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,8 @@
+// Task 1
+let purchaseAmount = 150;
+console.log(`Initial Ammount: $${purchaseAmount}`);
+
+    if (purchaseAmount > 100) {
+    purchaseAmount *= 0.9;}
+
+console.log(`Final amount after discount: $${purchaseAmount}`);
