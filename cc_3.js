a5709a8dfae40498aa2db507713239188824fb15
Cammarquez <cameronmarquez@usf.edu>
2025-02-01 09:28:12 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -6,3 +6,13 @@ console.log("Prices:", prices);
     prices.shift();
 
 console.log("Updated Prices:", prices);
+
+// Task 2
+let orders = [10, 20, 30, 40, 50];
+    orders[2] += 5;
+console.log("Orders:", orders);
+
+let totalOrders = orders.reduce((sum, order) => sum + order, 0);
+console.log("Updated Orders:", orders);
+console.log("Total Order Count:", totalOrders);
+
