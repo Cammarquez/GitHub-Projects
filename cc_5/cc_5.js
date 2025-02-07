eb932e477495182019e429c476c8a7891de08542
Cammarquez <cameronmarquez@usf.edu>
2025-02-07 14:27:01 -0500

    Task 1 - Customer Profile

---
+++ cc_5/cc_5.js
@@ -0,0 +1,9 @@
+//Task 1
+const customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"};// create an object called customer with the properties name, age and email
+
+console.log(`Name: ${customer.name}`);// log the name of the customer
+console.log(`Age: ${customer.age}`);// log the age of the customer
+console.log(`Email: ${customer.email}`);// log the email of the customer
