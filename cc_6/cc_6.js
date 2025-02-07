7aae15f13d105698e55752a736d744843c11e3d1
Cammarquez <cameronmarquez@usf.edu>
2025-02-07 16:24:50 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6/cc_6.js
+++ cc_6/cc_6.js
@@ -12,5 +12,30 @@ const calculateSalesTax = function(amount, taxRate) {
     const salesTax = amount * taxRate;
     return `Sales Tax: $${salesTax}`;}; // Function to calculate sales tax based on the amount and tax rate, return shows the result as a string when the calculateSalesTax function is called
 
-console.log(calculateSalesTax(100, 0.07)); // Expected output: "Sales Tax: $7"
-console.log(calculateSalesTax(500, 0.1));  // Expected output: "Sales Tax: $50"
+console.log(calculateSalesTax(100, 0.07)); // To log the result of the calculateSalesTax function when called
+console.log(calculateSalesTax(500, 0.1));  // To log the result of the calculateSalesTax function when called
+
+//Task 3
+const calculateBonus = (salary, performanceRating) => {
+    let bonusPercentage;
+
+    switch (performanceRating) {
+        case "Excellent":
+            bonusPercentage = 0.2;
+            break;
+        case "Good":
+            bonusPercentage = 0.1;
+            break;
+        case "Average":
+            bonusPercentage = 0.05;
+            break;
+        default:
+            bonusPercentage = 0;}
+
+    const bonus = salary * bonusPercentage;
+    return `Bonus: $${bonus}`;}; // Function to calculate bonus based on salary and performance rating, return shows the result as a string when the calculateBonus function is called
+
+console.log(`Salary: 5000, Performance Rating: "Excellent"`);
+console.log(calculateBonus(5000, "Excellent")); // Returns the bonus amount when the function is called
+console.log(`Salary: 7000, Performance Rating: "Good"`);
+console.log(calculateBonus(7000, "Good"));      // Returns the bonus amount when the function is called
