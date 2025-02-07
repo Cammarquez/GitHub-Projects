c9b36b21bcf4826d3926e7ca3fdff64cc9fb35ec
Cammarquez <cameronmarquez@usf.edu>
2025-02-07 16:06:27 -0500

    Task 2 - Sales Tax Computation

--- cc_6/cc_6.js
+++ cc_6/cc_6.js
@@ -6,3 +6,11 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 
 console.log(calculateProfit(20, 30, 100)); // Expected output: "Total Profit: $1000" 
 console.log(calculateProfit(50, 70, 200)); // Expected output: "Total Profit: $4000"
+
+//Task 2
+const calculateSalesTax = function(amount, taxRate) {
+    const salesTax = amount * taxRate;
+    return `Sales Tax: $${salesTax}`;}; // Function to calculate sales tax based on the amount and tax rate, return shows the result as a string when the calculateSalesTax function is called
+
+console.log(calculateSalesTax(100, 0.07)); // Expected output: "Sales Tax: $7"
+console.log(calculateSalesTax(500, 0.1));  // Expected output: "Sales Tax: $50"
