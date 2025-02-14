da23ef8d8a6673cd71541b286776f8636ff1025b
Cammarquez <cameronmarquez@usf.edu>
2025-02-14 13:52:13 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7/cc_7.js
+++ cc_7/cc_7.js
@@ -5,5 +5,13 @@ function calculateInvoice(subtotal, taxRate, discount) { // Defines the function
     const total = (subtotal + (subtotal * taxRate)) - discount;
     return `Total Invoice: $${total.toFixed(2)}`;} //puts the total in a string and formats it to 2 decimal places
 
-console.log(calculateInvoice(100, 0.08, 5)); // Expected output: "Total Invoice: $103.00"
-console.log(calculateInvoice(500, 0.1, 20)); // Expected output: "Total Invoice: $530.00"
+console.log(calculateInvoice(100, 0.08, 5)); // Calculates the total and logs it to the console
+console.log(calculateInvoice(500, 0.1, 20)); // calculates the total and logs it to the console
+
+// Task 2
+const calculateHourlyWage = function(salary, hoursPerWeek) { // Defines the function and sets varriables
+    const hourlyWage = salary / (hoursPerWeek * 52); // defines the hourly wage and creates the formula
+    return `Hourly Wage: $${hourlyWage.toFixed(2)}`;}; // puts the hourly wage in a string and formats it to 2 decimal places
+
+console.log(calculateHourlyWage(52000, 40)); // calculates the hourly wage and logs it to the console
+console.log(calculateHourlyWage(75000, 35)); // calculates the hourly wage and logs it to the console
