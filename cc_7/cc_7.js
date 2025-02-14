4c2c7fb13edd57366d002c1446301643dddb8394
Cammarquez <cameronmarquez@usf.edu>
2025-02-14 13:48:15 -0500

    Task 1 - Customer Invoice Calculation

--- cc_7/cc_7.js
+++ cc_7/cc_7.js
@@ -1,1 +1,9 @@
 //Initial commit - JavaScript Functions Challenge Setup
+
+// Task 1
+function calculateInvoice(subtotal, taxRate, discount) { // Defines the function and set varriables
+    const total = (subtotal + (subtotal * taxRate)) - discount;
+    return `Total Invoice: $${total.toFixed(2)}`;} //puts the total in a string and formats it to 2 decimal places
+
+console.log(calculateInvoice(100, 0.08, 5)); // Expected output: "Total Invoice: $103.00"
+console.log(calculateInvoice(500, 0.1, 20)); // Expected output: "Total Invoice: $530.00"
