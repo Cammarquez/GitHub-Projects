50342a6c7a031b50d7cf7b86ec012103f8842991
Cammarquez <cameronmarquez@usf.edu>
2025-02-14 14:57:32 -0500

    Task 2 - Product Price After Discount

--- cc_8/cc_8.js
+++ cc_8/cc_8.js
@@ -7,3 +7,11 @@ function calculateSalary(baseSalary, bonus, taxRate) { //to create a function an
 
 console.log(calculateSalary(5000, 500, 0.1)); //to call the function and calculate the net salary
 console.log(calculateSalary(7000, 1000, 0.15)); //to call the function and calculate the net salary
+
+// Task 2
+const calculateDiscount = function(price, discountRate) { //to create a function and set the variables
+    const finalPrice = price - (price * discountRate); //to calculate the final price after the discount and define the variables
+    return `Final Price: $${finalPrice.toFixed(2)}`;}; //to return the final price after the discount when the calculation is called.
+
+console.log(calculateDiscount(100, 0.2)); // to call the function and calculate the final price after the discount
+console.log(calculateDiscount(250, 0.15)); // to call the function and calculate the final price after the discount
